53f602b07fa9e6b602b46290e778c3daf7f62d48
pavel148 <20132305z@gmail.com>
2023-10-07 22:10:51 +0300

    Работающий калькулятор

--- HomeWork_2_Calculator/index.js
+++ HomeWork_2_Calculator/index.js
@@ -1,5 +1,6 @@
 
 let text = '';
+var result;
 function sym(x, y){
     return x+y;
 }
@@ -15,26 +16,42 @@ function division(x,y){
     return res;
 }
 
+function clearCalculator() {
+    console.log(1)
+    text = "";
+    result = 0;
+    document.getElementById('history_operations').textContent = "";
+    document.getElementById('conclusion').textContent = "";
+    document.getElementById('expression').value = "";
+}
+
 function get_number(id){
     
-    text += number;
+    text +=  document.getElementById(id).value;
     document.getElementById('expression').value = text;
 
 }
-
-function get_operation(id){
+function append(){
+    document.getElementById('history_operations').textContent += '\n';
+    document.getElementById('history_operations').textContent += result;
     
-    text += operator;
+
+}
+function get_operation(id){
+    text +=  id;
     document.getElementById('expression').value = text;
 }
 
 function calculate() {
-    let number1='';
-    let number2='';
     try {
-        result = eval(text);
-        document.getElementById('conclusion').textContent = result;
+        result = eval( document.getElementById('expression').value);
+        document.getElementById('conclusion').textContent = result ;
+        console.log(result)
+       append()
+       text=''
+       result=""
     } catch (error) {
         document.getElementById('conclusion').textContent = 'Error';
+        
     }
 }
